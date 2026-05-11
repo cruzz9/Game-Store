55b75418a13c6d797d9ff0641142b89dd0851fbb
paulina <cruzalvarezpaulina@gmail.com>
2026-05-11 17:09:05 -0600

    se agrega el index del programa

---
+++ index.js
@@ -0,0 +1,9 @@
+import {calculateTotal }from "./descuentos";
+import { calcularEnvio } from "./envio";
+import { vip } from "./vip";
+
+let name = prompt("Ingresa tu nombre:");
+console.log(productos);
+
+
+calculateTotal();
---
+++ vip.js
@@ -0,0 +1,8 @@
+let usuario = Boolean;
+export function vip(){
+    if(usuario > 20 && total > 1000 ){
+        console.log(true);
+    }else{
+        console.log(false);
+    }
+}
